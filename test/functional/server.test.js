6a10696626284cb0c8911a91d72772c67fe74624
Rafael Izidoro <izidoro.rafa@gmail.com>
2018-10-08 08:21:48 -0300

    fix tests

--- test/functional/server.test.js
+++ test/functional/server.test.js
@@ -23,9 +23,10 @@ it('GET /block/0', (done) => {
   setTimeout(async () => {
     let response = await server.hapi.inject({method: 'GET', url: '/block/0'})
     let data = JSON.parse(response.payload);
+    let genesis = {"address": "1JcnVEyQXVJQHBd2sFn3bt4XREixb4CxPF", "star": {"dec": 0, "ra": 0, "story": "5468652054696d65732030302f30302f303020546865204269672042616e67206a7573742068617070656e6564", "storyDecoded": "The Times 00/00/00 The Big Bang just happened"}}
 
     expect(response.statusCode).toEqual(200);
-    expect(data.body).toEqual('The Times 03/Jan/2009 Chancellor on brink of second bailout for banks');
+    expect(data.body).toEqual(genesis);
     expect(data.height).toEqual(0);
     expect(data.previousBlockHash).toEqual("");
     expect(data.hash).not.toBeNull();
@@ -40,13 +41,7 @@ it('POST /block/0', (done) => {
     let response = await server.hapi.inject({method: 'POST', url: '/block', payload: {body: "Satoshi is Alive"}})
     let data = JSON.parse(response.payload);
 
-    expect(response.statusCode).toEqual(200);
-    expect(data.body).toEqual('Satoshi is Alive');
-    expect(data.height).toEqual(1);
-    expect(data.previousBlockHash).not.toEqual("");
-    expect(data.hash).not.toBeNull();
-    expect(data.time).not.toBeNull();
-
+    expect(response.statusCode).toEqual(400);
     done();
   }, 1000)
 });
